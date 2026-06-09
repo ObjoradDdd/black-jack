3e762277591af8ca22cc437dcfea67ccb259cc69
ObjoradDdd <a.trunov2006@stud.tsu.ru>
2026-06-09 19:11:24 +0700

    (user-service) init

--- backend/user-service/src/main/java/com/github/objoraddd/blackjack/Main.java
+++ backend/user-service/src/main/java/com/github/objoraddd/blackjack/Main.java
@@ -1,7 +1,20 @@
 package com.github.objoraddd.blackjack;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+
 }
